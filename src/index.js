265feb015fa68e495e86083e57926fc87fa7b804
ayan4m1 <andrew@bulletlogic.com>
2022-07-31 18:18:58 -0400

    initial skeleton implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
+import { SerialPort } from 'serialport';
+
 import { getLogger } from './modules/logging.js';
 
 const log = getLogger('app');
 
-log.info('Hello World!');
+const execute = async () => {
+  log.info('Connecting...');
+  const port = new SerialPort({
+    path: 'COM3',
+    baudRate: 9600
+  });
+
+  port.on('open', (err) => {
+    if (err) {
+      log.error(err);
+      return;
+    }
+
+    port.write('F=1S\n');
+    log.info('Wrote fire command!');
+  });
+  port.on('data', (data) => {
+    console.dir(data);
+  });
+};
+
+execute();
